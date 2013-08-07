466bc4609979caff1c5a44ad649caef67bb17c4c
rus <russellxhart@googlemail.com>
2013-08-07 21:23:01 +0100

    Removing semicolons :-}

--- src/main/groovy/app/FuckOffRenderer.groovy
+++ src/main/groovy/app/FuckOffRenderer.groovy
@@ -8,8 +8,8 @@ import static groovy.json.JsonOutput.toJson
 import static org.ratpackframework.groovy.Util.with
 import static org.ratpackframework.groovy.templating.Template.groovyTemplate
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
 import com.itextpdf.text.pdf.PdfWriter
 
 class FuckOffRenderer extends ByTypeRenderer<FuckOff> {
@@ -36,24 +36,24 @@ class FuckOffRenderer extends ByTypeRenderer<FuckOff> {
         }
     }
 
-    byte[] toPdf(FuckOff f) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    static byte[] toPdf(FuckOff f) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
         Document document = new Document()
-        PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+        PdfWriter writer = PdfWriter.getInstance(document, outputStream)
 
         // Encrypt and set the password
         writer.setEncryption("fuckoff".bytes, "ownerfuckoff".bytes,
-                PdfWriter.ALLOW_PRINTING, PdfWriter.STANDARD_ENCRYPTION_128|PdfWriter.DO_NOT_ENCRYPT_METADATA);
-        writer.createXmpMetadata();
+                PdfWriter.ALLOW_PRINTING, PdfWriter.STANDARD_ENCRYPTION_128|PdfWriter.DO_NOT_ENCRYPT_METADATA)
+        writer.createXmpMetadata()
 
-        document.open();
-        document.addTitle("Fuck OFF");
-        document.addSubject("Fuck Off");
-        document.addKeywords("Fuck Off");
-        document.addAuthor("FOaaS");
-        document.addCreator("http://ratpack-foaas.herokuapp.com/");
-        document.add(new Paragraph("$f.message $f.subtitle"));
-        document.close();
+        document.open()
+        document.addTitle("Fuck OFF")
+        document.addSubject("Fuck Off")
+        document.addKeywords("Fuck Off")
+        document.addAuthor("FOaaS")
+        document.addCreator("http://ratpack-foaas.herokuapp.com/")
+        document.add(new Paragraph("$f.message $f.subtitle"))
+        document.close()
 
         return outputStream.toByteArray()
     }
